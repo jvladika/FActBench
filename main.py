41f82b913d23d72846f7529399e806ebf3958c22
Anum Afzal <anumafzaldogar@gmail.com>
2024-11-24 17:48:13 +0100

    updated code

--- main.py
+++ main.py
@@ -69,9 +69,9 @@ class GenFact:
 
                 if self.args.n_samples is not None and tot == args.n_samples:
                     break
-        #topics = topics[:2]
-        #generations = generations[:2]
-        #groundings = groundings[:2]
+        topics = topics[:20]
+        generations = generations[:20]
+        groundings = groundings[:20]
         out = self.fs.get_score(topics=topics,
                            generations=generations,
                            groundings=groundings,
