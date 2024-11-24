46ac005f40e35ee0ad1678243b283ce043c64c85
Anum Afzal <anumafzaldogar@gmail.com>
2024-11-24 21:35:40 +0100

    ..

--- main.py
+++ main.py
@@ -69,9 +69,9 @@ class GenFact:
 
                 if self.args.n_samples is not None and tot == args.n_samples:
                     break
-        #topics = topics[:20]
-        #generations = generations[:20]
-        #groundings = groundings[:20]
+        topics = topics[:1]
+        generations = generations[:1]
+        groundings = groundings[:1]
         out = self.fs.get_score(topics=topics,
                            generations=generations,
                            groundings=groundings,
@@ -506,6 +506,7 @@ if __name__ == '__main__':
         wandb_table = {"fs_wiki": factscore_out_vanilla["score"], "fs_grounded": factscore_out["score"],
                        "fs_grounded_wiki": fs_updated_score, "dummy_value": 0}
         wandb_push_json(wandb_table, table_name="fs_table")
+        fs_hal = ['\n'.join(hal) for hal in flatten_hallucinations(fs_updated_wrong_facts)]
 
 
     #Creates new class for deberta predictions. Loads a model from HuggingFace.
@@ -545,7 +546,7 @@ if __name__ == '__main__':
     #db_regenerations = regenerate_text(factscore_out["generations"], flatten_hallucinations(fs_updated_wrong_facts))
     db_regenerations = ['']*len(factscore_out["generations"]) #regenerate_text(factscore_out["generations"], flatten_hallucinations(fs_updated_wrong_facts))
 
-    wandb_table = {"generations": factscore_out["generations"], "hallucinations": fs_updated_wrong_facts,
+    wandb_table = {"generations": factscore_out["generations"], "fs_hallucinations": fs_hal,
                     'regenerations':db_regenerations}
     wandb_push_table(wandb_table)
 
